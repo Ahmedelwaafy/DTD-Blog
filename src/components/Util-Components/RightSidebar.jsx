09ecc74c9e803dc150c9ea8b92e13f1056cb3651
Ahmedelwaafy <Ahmedelwaafy@gmail.com>
2023-02-07 17:22:37 +0200

    finish posts feed section

--- src/components/Util-Components/RightSidebar.jsx
+++ src/components/Util-Components/RightSidebar.jsx
@@ -1,99 +1,198 @@
-import React, { useState } from "react";
+import React from "react";
 import { useNavigate } from "react-router-dom";
+import SidebarPost from "./SidebarPost";
+import Authors from "./Authors";
 
 function RightSidebar() {
   const navigate = useNavigate();
-  const [write, setWrite] = useState(true)
+
+  const posts = [
+    {
+      img: "../assets/hero-posts/1.jpg",
+      author: "Will Brett",
+      time: "November 3, 2022",
+      category: "web",
+      title: "11 Stages To Become A JavaScript Full-Stack Engineer",
+      description:
+        "web of the questions asked are about how to quickly improve their skills, how to become a Full Stack Developer, or how to choose a career direction.",
+      tags: "javascript",
+      long: "4min read",
+    },
+    {
+      img: "../assets/hero-posts/2.jpg",
+      author: "Nick Meyer",
+      time: "December 8, 2022",
+      category: "web",
+      title:
+        "8 design system management tools for startups & organizations, 2023",
+      description:
+        "Design systems create consistency, scalability, and efficiency across complex products and distributed teams. They’re also a drain on resources and a struggle to keep synced in dynamic organizations. These tools can help.",
+      tags: "management",
+      long: "10min read",
+    },
+    {
+      img: "../assets/hero-posts/3.jpg",
+      author: "Nirbhay luthra",
+      time: "Jan 31, 2023",
+      category: "web",
+      title: "How I Made My App 2.4x Faster Switching to Svelte",
+      description:
+        "This article is not to say that Angular can’t be fast. That’s not true. It’s that Svelte Kit makes it intuitive to be fast.",
+      tags: "Svelte",
+      long: "6min read",
+    },
+  ];
   return (
-    <aside className="RightSidebar">
-      {/**unlimited access*/}
-      <div className="RightSidebar__unlimited">
-        <div className="RightSidebar__unlimited--content">
-          <h2>Get unlimited access to everything on DTD</h2>
-          <p>Plans starting at less than $1/week.</p>
-          <button onClick={() => navigate("/pricing")}>
-            Become a member <img src="../assets/star2.svg " alt="star" />
+    <div className="LeftSidebar">
+      {/**Most Read*/}
+      <div className="LeftSidebar__most">
+        <div className="LeftSidebar__most--title">
+          <h2>MOST READ</h2>
+          <button>
+            See all <img src="../assets/right-arrow.svg" alt="right-arrow" />
           </button>
         </div>
-        <img
-          className="RightSidebar__unlimited--img"
-          src="/assets/premium.svg"
-          alt="premium"
-        />
-      </div>
-      <hr />
-      {/**ADS*/}
-      <div className="RightSidebar__ads">
-        <h3 className="RightSidebar__ads--title">
-          <img src="../assets/dot.svg" alt="dot" /> ADS
-        </h3>
-        <div className="RightSidebar__ads--content">
-          <h2>250X250</h2>
-          <p>SQUARE ADS</p>
-          <p className="place">Place your ADS on DTD and reach more people.</p>
-          <img src="../assets/ads.svg" alt="ads" />
-          <button className="link"> Contact Sales</button>
+
+        <div className="LeftSidebar__most--content">
+          <SidebarPost
+            post={posts[0]}
+            position={"Fron-End Developer"}
+            imgno={"1"}
+          />
+          <SidebarPost
+            post={posts[1]}
+            position={"Full-Stack Developer"}
+            imgno={"2"}
+          />
+          <SidebarPost post={posts[2]} position={"UI Designer"} imgno={"3"} />
         </div>
       </div>
-      <hr />
-      {/**Hot Tags*/}
-      <div className="RightSidebar__tags">
-        <h3 className="RightSidebar__ads--title">
-          <img src="../assets/dot.svg" alt="dot" /> HOT TAGS
-        </h3>
-        <div className="RightSidebar__tags--content">
-          <button>#React</button>
-          <button>#Layoffs</button>
-          <button>#TypeScript </button>
-          <button>#GitHub </button>
-          <button>#CICD</button>
-          <button>#Tailwind</button>
-          <button>#ORM</button>
-          <button>#Prisma </button>
-          <button>#Next.js</button>
-          <button>#JavaScript</button>
-          <button>#Remote </button>
-          <button>#Roadmap </button>
+
+      {/**Web Category*/}
+      <div className="LeftSidebar__web">
+        <div className="LeftSidebar__web--title">
+          <h2>WEB</h2>
+          <button>
+            See all <img src="../assets/right-arrow.svg" alt="right-arrow" />
+          </button>
+        </div>
+
+        <div className="LeftSidebar__web--content">
+          <SidebarPost
+            post={posts[0]}
+            position={"Fron-End Developer"}
+            imgno={"1"}
+          />
+          <SidebarPost
+            post={posts[1]}
+            position={"Full-Stack Developer"}
+            imgno={"2"}
+          />
+          <SidebarPost post={posts[2]} position={"UI Designer"} imgno={"3"} />
+        </div>
+        <button className="LeftSidebar__web--button">
+          {" "}
+          See All Categories{" "}
+        </button>
+      </div>
+
+      {/**Best Authors*/}
+      <div className="LeftSidebar__authors">
+        <div className="LeftSidebar__authors--title">
+          <h2>BEST AUTHORS</h2>
+          <button>
+            See all <img src="../assets/right-arrow.svg" alt="right-arrow" />
+          </button>
+        </div>
+
+        <div className="LeftSidebar__authors--content">
+          <Authors
+            post={posts[0]}
+            position={"Fron-End Developer"}
+            imgno={"1"}
+          />
+          <Authors
+            post={posts[1]}
+            position={"Full-Stack Developer"}
+            imgno={"2"}
+          />
+          <Authors post={posts[2]} position={"UI Designer"} imgno={"3"} />
         </div>
       </div>
-      <hr />
-      {/**Instagram*/}
-      <div className="RightSidebar__Instagram">
-        <h3 className="RightSidebar__Instagram--title">
-          <img src="../assets/dot-rose.svg" alt="dot" /> INSTAGRAM
-        </h3>
-        <div className="RightSidebar__Instagram--content">
-          <img src="../assets/instagram/insta1.jpg" alt="insta" />
-          <img src="../assets/instagram/insta2.jpg" alt="insta" />
-          <img src="../assets/instagram/insta3.jpg" alt="insta" />
-          <img src="../assets/instagram/insta4.jpg" alt="insta" />
-          <img src="../assets/instagram/insta5.jpg" alt="insta" />
-          <img src="../assets/instagram/insta6.jpg" alt="insta" />
+
+      {/**Quick Survey*/}
+      <div className="LeftSidebar__survey">
+        <div className="LeftSidebar__survey--title">
+          <h2>QUICK SURVEY</h2>
+          <button>
+            See results{" "}
+            <img src="../assets/right-arrow.svg" alt="right-arrow" />
+          </button>
+        </div>
+
+        <div className="LeftSidebar__survey--content">
+          <h3>How was your experience on DTD?</h3>
+          <form action="">
+            <div>
+              <input
+                type="radio"
+                id="cbx1"
+                className="cbx"
+                name="survey"
+                style={{ display: "none" }}
+              />
+              <label htmlFor="cbx1" className="check">
+                <svg width="18px" height="18px" viewBox="0 0 18 18">
+                  <path d="M 1 9 L 1 9 c 0 -5 3 -8 8 -8 L 9 1 C 14 1 17 5 17 9 L 17 9 c 0 4 -4 8 -8 8 L 9 17 C 5 17 1 14 1 9 L 1 9 Z"></path>
+                  <polyline points="1 9 7 14 15 4"></polyline>
+                </svg>
+                Awesome, I'm satisfied!
+              </label>
+            </div>
+            <div>
+              <input
+                type="radio"
+                id="cbx2"
+                className="cbx"
+                name="survey"
+                style={{ display: "none" }}
+              />
+              <label htmlFor="cbx2" className="check">
+                <svg width="18px" height="18px" viewBox="0 0 18 18">
+                  <path d="M 1 9 L 1 9 c 0 -5 3 -8 8 -8 L 9 1 C 14 1 17 5 17 9 L 17 9 c 0 4 -4 8 -8 8 L 9 17 C 5 17 1 14 1 9 L 1 9 Z"></path>
+                  <polyline points="1 9 7 14 15 4"></polyline>
+                </svg>
+                Normal
+              </label>
+            </div>
+            <div>
+              <input
+                type="radio"
+                id="cbx3"
+                className="cbx"
+                name="survey"
+                style={{ display: "none" }}
+              />
+              <label htmlFor="cbx3" className="check">
+                <svg width="18px" height="18px" viewBox="0 0 18 18">
+                  <path d="M 1 9 L 1 9 c 0 -5 3 -8 8 -8 L 9 1 C 14 1 17 5 17 9 L 17 9 c 0 4 -4 8 -8 8 L 9 17 C 5 17 1 14 1 9 L 1 9 Z"></path>
+                  <polyline points="1 9 7 14 15 4"></polyline>
+                </svg>
+                Bad! Need mor improvement
+              </label>
+            </div>
+
+            <button
+            className="submit-btn" type="submit">
+             Submit
+            </button>
+          </form>
+          <p>
+            12,523 <span>Readers joined</span>
+          </p>
         </div>
       </div>
-      <hr />
-      {/**Write*/}
-      {write && (
-        <div className="RightSidebar__write">
-          <div className="RightSidebar__write--close">
-            <h3 className="title">
-              <img src="../assets/dot-purple.svg" alt="dot" /> WRITE ON DTD
-            </h3>
-            <img
-              onClick={() => setWrite(false)}
-              src="../assets/close.svg"
-              alt="close"
-            />
-          </div>
-          <div className="RightSidebar__write--content">
-            <button>New blogger FAQ</button>
-            <button>Pro blogging advices</button>
-            <button>Maximize your readership</button>
-            <button className="start">Start Writing</button>
-          </div>
-        </div>
-      )}
-    </aside>
+    </div>
   );
 }
 
