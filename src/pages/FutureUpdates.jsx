93aa413f0759fd68527fb4a0cbaa57ebe662be41
Ahmedelwaafy <Ahmedelwaafy@gmail.com>
2023-02-20 17:07:47 +0200

    add favicon

--- src/pages/FutureUpdates.jsx
+++ src/pages/FutureUpdates.jsx
@@ -4,14 +4,10 @@ function FutureUpdates() {
   return (
     <div className="flex items-center justify-center w-full h-screen">
       <ul className="w-96 flexCol gap-2 items-start">
-        <li>#Improve Pagination Mechanism </li>
-        <li>
-          # Separate the State of Each Page, As Now All Pages Share the Global
-          State Which Get from the FilterSlice, Ex. the Filter Set in Women Page
-          is Applied to The Men Page Also
-        </li>
-        <li>#Improve Layout Shift Metric</li>
-        <li>#Implement Stripe for Checkout</li>
+        <li># Pagination/Show More.</li>
+        <li>#Add Authentication with Facebook and GitHub Providers.</li>
+        <li>#Add More Functionalities like Night Mode, Comment, Like...etc</li>
+        <li>#Implement Redux to Store The State Globally </li>
       </ul>
     </div>
   );
